81bddfa29aa42c39451dfddf367a0b4ef79b00af
William Jagels <will.jagels@gmail.com>
2015-01-17 15:37:33 -0500

    Yelp work

--- apis/yelp.js
+++ apis/yelp.js
@@ -1,26 +1,32 @@
 var api = {}
 
+var keys = require('../keys.js')
 var yelp = require("yelp").createClient({
 	consumer_key: keys.YELP_CONSUMER_KEY, 
 	consumer_secret: keys.YELP_CONSUMER_SECRET,
 	token: keys.YELP_TOKEN,
 	token_secret: keys.YELP_TOKEN_SECRET
 });
-var keys = require('../keys.js')
+var freegeoip = require('node-freegeoip');
 
 api.findSnacks = {
-	phrases: ["i want food", "feed me", "hungry", "dinner", "lunch", "breakfast", "snacks"],
-	call_api: function(rawText, callback) {
-		// See http://www.yelp.com/developers/documentation/v2/search_api
-		yelp.search({term: "food", location: "Montreal"}, function(error, data) {
-			console.log(error);
-			console.log(data);
+	phrases: ["i want food", "feed me", "feed", "hungry", "dinner", "lunch", "breakfast", "snacks"],
+	call_api: function(rawText, callback, req) {
+		freegeoip.getLocation(req.ip, function(err, location) {
+			console.log(location);
+			// See http://www.yelp.com/developers/documentation/v2/search_api
+			yelp.search({term: "food", location: location['zip_code']}, function(error, data) {
+				console.log(error);
+				console.log(data);
+				callback(data);
+			});
 		});
 	}
 }
-
+/*
 // See http://www.yelp.com/developers/documentation/v2/business
 yelp.business("yelp-san-francisco", function(error, data) {
 	console.log(error);
 	console.log(data);
 });
+*/

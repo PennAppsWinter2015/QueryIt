96d63841d901c03e3b398ed003600a48f7ae1086
Dan <dannysilver3@gmail.com>
2015-01-17 17:20:08 -0500

    json not xml for usa today

--- apis/usatoday.js
+++ apis/usatoday.js
@@ -6,7 +6,7 @@ var keys = require('../keys.js');
 api.topNews = {
   phrases: ['breaking news', 'todays news','current news', 'news', 'todays news'],
   call_api: function(raw_text, callback) {
-    request('http://api.usatoday.com/open/articles/topnews?encoding=JSON?api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
+    request('http://api.usatoday.com/open/articles/topnews?encoding=JSON&api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         callback(body)
       } else {
@@ -18,7 +18,7 @@ api.topNews = {
 api.lifeNews = {
   phrases: ['entertainment','kim kardashian','reality tv','movie','box office film Fox people','TMZ Actor','Actress love','ex','dating','videos','music','critics marriage','celebrity','gossip','fake','awards','emmys','oscars presentation vma golden globes'],
   call_api: function(raw_text, callback) {
-      request('http://api.usatoday.com/open/articles/topnews/life?api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
+      request('http://api.usatoday.com/open/articles/topnews/life?encoding=JSON&api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         callback(body)
       } else {
@@ -30,7 +30,7 @@ api.lifeNews = {
 api.moneyNews = {
   phrases: ['money','stocks','buy','sell','economy banks federal reserve bull','wall street','stock market','rating','DOW Jones','NASDAQ','S&P 500','Financial News','Unemployment','GDP','Economy','Economics','Bonds','Mutual Funds'],
   call_api: function(raw_text, callback) {
-      request('http://api.usatoday.com/open/articles/topnews/money?api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
+      request('http://api.usatoday.com/open/articles/topnews/money?encoding=JSON&api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         callback(body)
       } else {
@@ -43,7 +43,7 @@ api.moneyNews = {
 api.sportsNews = {
   phrases: ['sports','sports news','football','basketball','NFL NBA MLB MLS','soccer','Super Bowl','Coach','Team','Score','Fans','World Series','World Cup','Olympics','Tennis','Golf','Swimming','Cricket'],
   call_api: function(raw_text, callback) {
-    request('http://api.usatoday.com/open/articles/topnews/sports?api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
+    request('http://api.usatoday.com/open/articles/topnews/sports?encoding=JSON&api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         callback(body)
       } else {
@@ -56,7 +56,7 @@ api.sportsNews = {
 api.techNews = {
   phrases: ['technology','tech news','apple','social media','facebook','google','apps','iPhone','Android','smartphone','computer','laptop','chromebook'],
   call_api: function(raw_text, callback) {
-    request('http://api.usatoday.com/open/articles/topnews/tech?api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
+    request('http://api.usatoday.com/open/articles/topnews/tech?encoding=JSON&api_key=' + keys.USATODAY_ARTICLES_KEY, function (error, response, body) {
       if (!error && response.statusCode == 200) {
         callback(body)
       } else {

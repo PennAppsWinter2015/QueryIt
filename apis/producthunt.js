b08d0fddf106dc8f056f44517b7c52241d1a7569
Anuraag Yachamaneni <hello@anuraag.me>
2015-01-17 04:11:43 -0500

    add producthunt.js

---
+++ apis/producthunt.js
@@ -0,0 +1,17 @@
+var api = {}
+
+api.todaysProducts = {
+	phrases: ['coolest products', 'todays tech'],
+	call_api: function() {
+		console.log('fetching producthunt products of today')
+	}
+}
+
+api.topProductsAllTime = {
+	phrases: ['best apps of the year', 'coolest websites of the year'],
+	call_api: function() {
+		console.log('fetching top product hunts of the year')
+	}
+}
+
+module.exports = api;

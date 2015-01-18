7e730634bb4e3e119bc47574659d4c04f9c1a83a
Dan <dannysilver3@gmail.com>
2015-01-18 08:20:26 -0500

    trending on twitter

--- apis/twitter.js
+++ apis/twitter.js
@@ -2,6 +2,8 @@ var api = {}
 
 var Twit = require('twit')
 var keys = require('../keys.js')
+var request = require('request')
+
 
 var T = new Twit({
     consumer_key:         keys.TWITTER_CONSUMER_KEY
@@ -11,20 +13,43 @@ var T = new Twit({
 })
 
 api.trending = {
-	phrases: ['trending on twitter', 'popular on twitter'],
-	call_api: function(rawSearchText, callback) {
-		T.get('trends/place', {id : "1"}, function(err, data, response) {
-		    if (err != null) console.log("error", err)
-		  callback(data)
-		})
-	}
+  phrases: ['trending on twitter worldwide', 'popular on twitter worldwide', 'popular on twitter everywhere'],
+  call_api: function(rawSearchText, callback) {
+    T.get('trends/place', {id : "1"}, function(err, data, response) {
+        if (err != null) console.log("error", err)
+      callback(data)
+    })
+  }
 }
 
-api.top_users = {
-	phrases: ['top twitter accounts', 'twitter top users','top accounts', 'most popular users'],
-	call_api: function() {
-		console.log('fetching top twitter users')
-	}
+api.trending_location = {
+  phrases: ['trending tweets in the us', 'trending tweets', 'popular tweets'],
+  call_api: function(rawSearchText, callback) {
+    location = replaceStringsWithBlank(['trending', 'tweets', 'in'], rawSearchText)
+    request('https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=' + keys.GOOGLE_GEO,
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          loc = JSON.parse(body)
+          console.log(location)
+          console.log(loc)
+          geoLocation = loc["results"][0]["geometry"]["location"]
+          T.get('trends/closest', {lat : geoLocation["lat"], long: geoLocation["lng"]}, function(err, data, response) {
+            if (err != null) console.log("error", err)
+            T.get('trends/place', {id : data[0].woeid}, function(err, trendingData, response) {
+              if (err != null) console.log("error", err)
+                callback(trendingData)
+            })
+          })
+        }
+    })
+
+  }
 }
 
+function replaceStringsWithBlank(strings, string) {
+    for (var i = 0; i < strings.length; i++) {
+        string = string.replace(strings[i], "")
+    };
+    return string;
+}
 module.exports = api;

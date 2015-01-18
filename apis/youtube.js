a930f978259da3e411439b849509ba468cd14eed
Dan <dannysilver3@gmail.com>
2015-01-17 19:27:24 -0500

    update

--- apis/youtube.js
+++ apis/youtube.js
@@ -10,32 +10,36 @@ Youtube.authenticate({
 
 
 api.top_videos = {
-	phrases: ['popular videos', 'popular youtube videos'],
-	call_api: function(rawSearchText, callback) {
-		Youtube.search.list({
-		    "part": "id, snippet"
-		  , "maxResults": 5
-		  , "order": "viewCount"
-		  , "type": "video"
-		}, function (err, data) {
-		    if (err != null) console.log("error", err)
-		    callback(data)
-		});
-	}
-    
-api.channel_search = {
-	phrases: ['videos', 'video'],
-	call_api: function(rawSearchText, callback) {
-		Youtube.search.list({
-		    "part": "id, snippet"
-		  , "maxResults": 50
-		  , "type": "video"
-          , "q": rawSearchText
-		}, function (err, data) {
-		    if (err != null) console.log("error", err)
-		    callback(data)
-		});
-	}
+  phrases: ['popular videos', 'popular youtube videos'],
+  call_api: function(rawSearchText, callback) {
+    Youtube.search.list({
+        "part": "id, snippet"
+      , "maxResults": 5
+      , "order": "viewCount"
+      , "type": "video"
+    }, function (err, data) {
+        if (err != null) console.log("error", err)
+        callback(data)
+    });
+  }
+}
+
+api.channel_search = {}    
+api.channel_search.phrases = ['videos by', 'video'];
+
+api.channel_search.call_api = function(rawSearchText, callback) {
+    for (var i = 0; i < api.channel_search.phrases.length; i++) {
+      rawSearchText = rawSearchText.replace(api.channel_search.phrases[i], '')
+    };
+  Youtube.search.list({
+      "part": "id, snippet"
+    , "maxResults": 50
+    , "type": "video"
+        , "q": rawSearchText
+  }, function (err, data) {
+      if (err != null) console.log("error", err)
+      callback(data)
+  });
 }
 
 module.exports = api;
